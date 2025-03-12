80877843b256c6f7dc5c28888974c0a38344562f
1111234003 <evachuang1201@gmail.com>
2025-03-12 15:46:46 +0800

    Add files via upload

--- 1111234003/w4/w4/Source.cpp
+++ 1111234003/w4/w4/Source.cpp
@@ -1,103 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <iostream>
+#include <cmath>  // 用來計算圓柱體的 PI 值
 
-typedef struct stu
-{
-    int id;
-    char name[20];
-    int math;
-    int com;
-    float avg;
-    struct stu* next;
-}stu;
+using namespace std;
 
-void create(stu*& head, stu in);
-void out(stu* head);
-void free(stu*& head, int target);
-
-int main()
-{
-    stu* head = NULL;
-    stu in = { 0,"",0,0,0.0,NULL };
-    int target;
-
-    printf("input id: ");
-    scanf_s("%d", &in.id);
-    while (in.id != 0)
-    {
-        printf("input name、math、computer :");
-        scanf_s("%s%d%d", in.name, 19, &in.math, &in.com);
-        in.avg = (float)(in.math + in.com) / 2;
-        create(head, in);
-        printf("input id: ");
-        scanf_s("%d", &in.id);
-    }
-    out(head);
-
-    printf("\ninput id that you want to delete: ");
-    scanf_s("%d", &target);
-    free(head, target);
-
-
-    out(head);
-
-    return  0;
-
-}
-void create(stu*& head, stu in)
-{
-    stu* newnode = (stu*)malloc(sizeof(stu));
-    *newnode = in;
-
-    if (head == NULL || newnode->id < head->id)
-    {
-        newnode->next = head;
-        head = newnode;
-        return;
-    }
-
-    stu* temp = head;
-    while (temp->next != NULL && temp->next->id < newnode->id)
-    {
-        temp = temp->next;
-    }
-    newnode->next = temp->next;
-    temp->next = newnode;
+// 1. 輸入1個整數時，計算正方體體積
+double volume(int side) {
+    return pow(side, 3);  // 正方體體積 = 邊長的立方
 }
 
-void out(stu* head)
-{
-    printf("\nid     name      math        computer        avg\n");
-    stu* temp = head;
-    while (temp != NULL)
-    {
-        printf("\n%-10d%-10s%-10d%-10d%-34f\n", temp->id, temp->name, temp->math, temp->com, temp->avg);
-        temp = temp->next;
-    }
+// 2. 輸入2個整數時，計算圓柱體體積
+double volume(double radius, double height) {
+    const double PI = 3.14159265358979;  // 圓周率
+    return PI * pow(radius, 2) * height;  // 圓柱體體積 = π * 半徑平方 * 高度
 }
 
-void free(stu*& head, int target)
-{
-    stu* temp = head, * del = head;
-    if (del->id == target)
-    {
-        head = head->next;
-        free(del);
-        return;
+// 3. 輸入3個整數時，計算長方體體積
+double volume(int length, int width, int height = 10) {  // 預設高度為10
+    return length * width * height;  // 長方體體積 = 長 * 寬 * 高
+}
+
+int main() {
+    int choice;
+    cout << "選擇計算的體積:\n";
+    cout << "1. 正方體\n";
+    cout << "2. 圓柱體\n";
+    cout << "3. 長方體\n";
+    cout << "請選擇 (1/2/3): ";
+    cin >> choice;
+
+    if (choice == 1) {
+        int side;
+        cout << "請輸入正方體的邊長: ";
+        cin >> side;
+        cout << "正方體的體積為: " << volume(side) << endl;
     }
-    for (int i = 0; i <= target; i++)
-    {
-        if (del->id == target)
-        {
-            while (temp->next->id < del->id)
-            {
-                temp = temp->next;
-            }
-            temp->next = del->next;
-            free(del);
-            return;
-        }
-        del = del->next;
+    else if (choice == 2) {
+        double radius, height;
+        cout << "請輸入圓柱體的半徑和高度: ";
+        cin >> radius >> height;
+        cout << "圓柱體的體積為: " << volume(radius, height) << endl;
     }
-    printf("\nnot found!!!\n");
+    else if (choice == 3) {
+        int length, width, height;
+        cout << "請輸入長方體的長、寬、高 (若無高，則會使用預設值 10): ";
+        cin >> length >> width >> height;
+        cout << "長方體的體積為: " << volume(length, width, height) << endl;
+    }
+    else {
+        cout << "無效的選擇!" << endl;
+    }
+
+    return 0;
 }
