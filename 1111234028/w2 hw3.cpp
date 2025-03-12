b30d3ed61221070e599176d06bb403f339102461
1111234028 <148665955+jason970512@users.noreply.github.com>
2025-03-12 15:55:17 +0800

    reuplord w1~w2

---
+++ 1111234028/w2 hw3.cpp
@@ -0,0 +1,45 @@
+
+// Fig. 11.14: fig11_14.c
+// Reading a random-access file sequentially
+#include <stdio.h>
+
+// clientData structure definition               
+typedef struct clientData {
+	unsigned int ID; // account number
+	char Name[15]; // account last name    
+	int math, computer; // account first name   
+	double AUG; // account balance   
+}stu;
+
+int main(void)
+{
+	FILE* cfPtr; // accounts.dat file pointer
+
+
+	if (fopen_s(&cfPtr, "student.dat", "rb") != 0) {
+		puts("File could not be opened.\n");
+	}
+	else {
+		int searchID = 0;
+		printf("輸入您想搜尋資料的ID\n");
+		scanf_s("%d", &searchID);
+
+		while (!feof(cfPtr)) {
+
+			stu client = { 0, "",0,0, 0.0 };
+			int result = fread(&client, sizeof(struct clientData), 1, cfPtr);
+
+			if (result != 0 && client.ID != 0) {
+				if (client.ID == searchID)
+				{
+					printf("%6s%16s%11s%15s%10s\n", "Acct", "Name", "Math",
+						"computer", "AUG");
+					printf("%6d%14s%13d%10d%20lf\n",
+						client.ID, client.Name, client.math, client.computer, client.AUG);
+				}
+			}
+		}
+
+		fclose(cfPtr);
+	}
+}
