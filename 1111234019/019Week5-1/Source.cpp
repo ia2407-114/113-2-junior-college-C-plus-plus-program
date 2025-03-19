d4ca4b1edae87b055b6b0e7756e1d5ac2d50c381
BenShen0618 <benshen241@gmail.com>
2025-03-19 15:53:56 +0800

    Add files via upload

---
+++ 1111234019/019Week5-1/Source.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+class GradeBook
+{
+private:
+    char gender = 'm';
+    int age = 50;
+public:
+    
+    void displayMessage() const
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+    }
+    int printAge() const
+    {
+        return age;
+    }
+}; 
+
+
+int main()
+{
+    GradeBook myGradeBook;
+    myGradeBook.displayMessage(); 
+    cout << myGradeBook.printAge()<< endl;
+} 
