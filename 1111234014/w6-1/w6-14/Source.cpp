7a8df67b4921833d2ed5641f97edfdd50044295a
1111234014 <xuzining794@gmail.com>
2025-03-26 15:28:51 +0800

    Add files via upload

---
+++ 1111234014/w6-1/w6-14/Source.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+
+
+using namespace std;
+
+class D
+{
+	/*explicit D(string n, int a)
+	{
+		name = n;
+		age = a;
+	}*/
+
+public:
+	void setage(int a)
+	{
+		if (a < 20 && a >= 1)
+		{
+			age = a;
+
+		}
+		else
+		{
+			cout << "資料有誤不得大於20    ";
+		}
+
+	}
+
+	int getage()
+	{
+		return age;
+	}
+	void setname(string n)
+	{
+		name = n;
+	}
+	string getname()
+	{
+		return name;
+	}
+	void display()
+	{
+		cout << "dog age :" << getage() << endl;
+		cout << "dog name :" << getname() << endl;
+
+	}
+
+private:
+	int age;
+	string name;
+
+};
+
+int main()
+{
+	D dog;
+	int a;
+	string n;
+
+	cout << "input age: ";
+	cin >> a;
+
+	cout << "input name: ";
+	cin >> n;
+
+
+	dog.setage(a);
+	dog.setname(n);
+
+	dog.display();
+
+}
