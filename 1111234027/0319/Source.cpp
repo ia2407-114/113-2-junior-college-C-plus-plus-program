8f3c3a6466ae6319f31f1f390435af23f4e2f151
1111234027 <cannanjiang@gmail.com>
2025-04-02 13:40:42 +0800

    Add files via upload

---
+++ 1111234027/0319/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class GradeBook
+{
+private:
+    int myID = 50;
+public:
+    void changeID(int ID)
+    {
+        myID = ID;
+    }
+
+    int getID() const
+    {
+        return myID;
+    }
+
+    void displayMessage() const
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+    }
+};
+
+int main()
+{
+    GradeBook myGradeBook;
+    int id;
+
+    cout << "ID¬°:" << myGradeBook.getID() << endl;
+
+    cout << "½Ð¿é¤JID:";
+    cin >> id;
+
+    myGradeBook.changeID(id);
+    cout << "ID¬°:" << myGradeBook.getID() << endl;
+}
