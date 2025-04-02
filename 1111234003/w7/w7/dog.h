d03c501071793a9907012db7b7eaa0afcce74c5e
1111234003 <evachuang1201@gmail.com>
2025-04-02 15:29:19 +0800

    Add files via upload

---
+++ 1111234003/w7/w7/dog.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class D
+{
+public:
+	explicit D(string n, int a);
+	void setage(int a);
+	int getage();
+	void setname(string n);
+	string getname();
+	void display();
+private:
+	int age;
+	string name;
+};
