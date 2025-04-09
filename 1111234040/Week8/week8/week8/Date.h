f457a4aa794e6a83d8adaffd0deebe3bd424d89e
1111234040 <n0729a3@gmail.com>
2025-04-09 15:58:09 +0800

    Add files via upload

---
+++ 1111234040/Week8/week8/week8/Date.h
@@ -0,0 +1,30 @@
+﻿// Fig. 9.4: Time.h
+// Time class containing a constructor with default arguments.
+// Member functions defined in Time.cpp.
+// prevent multiple inclusions of header
+#ifndef DATE_H
+#define DATE_H
+
+class Date
+{
+public:
+	explicit Date(int = 1990, int = 1, int = 1);
+
+	void setDate(int, int, int);
+	void setYear(int);
+	void setMonth(int);
+	void setDay(int);
+
+	unsigned int getYear() const;
+	unsigned int getMonth() const;
+	unsigned int getDay() const;
+
+	void printUniversal() const; // ¦è¤¸
+	void printStandard() const; // ¥Á°ê
+private:
+	unsigned int year;
+	unsigned int month;
+	unsigned int day;
+};
+
+#endif
---
+++ 1111234040/Week8/week8/week8/implement.cpp
@@ -0,0 +1,74 @@
+﻿// Fig. 9.4: Time.h
+// Time class containing a constructor with default arguments.
+// Member functions defined in Time.cpp.
+
+// prevent multiple inclusions of header
+#include <iostream>
+#include <iomanip>
+#include <stdexcept>
+#include "Date.h"
+using namespace std;
+
+Date::Date(int year, int month, int day)
+{
+    setDate(year, month, day);
+}
+
+void Date::setDate(int year, int month, int day)
+{
+    setYear(year);
+    setMonth(month);
+    setDay(day);
+}
+
+void Date::setYear(int y)
+{
+    if (y >= 1911 && y <= 2050)
+        year = y;
+    else
+        throw invalid_argument("請輸入1911-2050");
+}
+
+void Date::setMonth(int m)
+{
+    if (m >= 1 && m <= 12)
+        month = m;
+    else
+        throw invalid_argument("請輸入1-12");
+}
+
+void Date::setDay(int d)
+{
+    if (d >= 1 && d <= 31)
+        day = d;
+    else
+        throw invalid_argument("請輸入1-31");
+}
+
+unsigned int Date::getYear() const
+{
+    return year;
+}
+
+unsigned int Date::getMonth() const
+{
+    return month;
+}
+
+unsigned int Date::getDay() const
+{
+    return day;
+}
+
+void Date::printUniversal() const
+{
+    cout << setfill('0') << "¦è¤¸" << getYear() << "¦~"
+        << setw(2) << getMonth() << "¤ë" << setw(2) << getDay();
+}
+
+void Date::printStandard() const
+{
+    cout << setfill('0') << "¥Á°ê" << getYear() - 1911 << "¦~"
+        << setw(2) << getMonth() << "¤ë" << setw(2) << getDay();
+}
+
