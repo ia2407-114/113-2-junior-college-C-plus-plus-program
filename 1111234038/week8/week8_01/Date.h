44a1f3f88ab7366c5a4d0091c976f80eb7037640
andyliu300111 <andyliu300111@gmail.com>
2025-04-09 15:24:01 +0800

    week8

---
+++ 1111234038/week8/week8_01/Date.h
@@ -0,0 +1,34 @@
+// Fig. 9.4: Time.h
+// Time class containing a constructor with default arguments.
+// Member functions defined in Time.cpp.
+
+// prevent multiple inclusions of header 
+#ifndef DATE_H
+#define DATE_H
+
+// Time class definition
+class Date
+{
+public:
+	explicit Date(int = 1990, int = 1, int = 1); // default constructor
+
+	// set functions
+	void setDate(int, int, int); // set hour, minute, second
+	void setYear(int); // set hour (after validation)
+	void setMonth(int); // set minute (after validation)
+	void setDay(int); // set second (after validation)
+
+	// get functions
+	unsigned int getYear() const; // return hour
+	unsigned int getMonth() const; // return minute
+	unsigned int getDay() const; // return second
+
+	void printUniversal() const; // 西元
+	void printStandard() const; // 民國
+private:
+	unsigned int year; 
+	unsigned int month; 
+	unsigned int day; 
+}; // end class Time
+
+#endif
