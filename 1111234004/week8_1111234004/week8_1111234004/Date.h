b914fe4465adbee7565185fd0c58028bccde0482
1111234004 <sueyu970516@gmail.com>
2025-04-10 11:52:41 +0800

    Add files via upload

---
+++ 1111234004/week8_1111234004/week8_1111234004/Date.h
@@ -0,0 +1,26 @@
+#ifndef DATE_H
+#define DATE_H
+
+class Date
+{
+public:
+	explicit Date(int = 1990, int = 1, int = 1);
+
+	void setDate(int, int, int);
+	void setYear(int);
+	void setMonth(int);
+	void setDay(int);
+
+	unsigned int getYear() const;
+	unsigned int getMonth() const;
+	unsigned int getDay() const;
+
+	void printUniversal() const;
+	void printStandard() const; 
+private:
+	unsigned int year;
+	unsigned int month;
+	unsigned int day;
+};
+
+#endif
