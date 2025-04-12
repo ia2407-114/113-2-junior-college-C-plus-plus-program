5a8fc31be1621f5a3fc8aca7acf8f668558a3a89
1111234033 <xiaoen970201@gmail.com>
2025-04-12 15:07:33 +0800

    Add files via upload

---
+++ 1111234033/W7-1/W7-1/implement.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <string>
+#include "Dog.h"
+using namespace std;
+
+Dog::Dog(string n, int a) {
+    name = n;
+    age = a;
+}
+
+void Dog::setInfo(string n, int a)
+{
+    if (a > 0 && a < 20) {
+        if (n.size() <= 10) {
+            name = n;
+            age = a;
+        }
+        else if (n.size() == 0) {
+            cout << "無效的名稱" << endl;
+        }
+        else {
+            name = n.substr(0, 10);
+            age = a;
+        }
+    }
+    else {
+        cout << "無效的年齡!請輸入0-20" << endl;
+    }
+}
+
+string Dog::getName() const
+{
+    return name;
+}
+
+int Dog::getAge() const
+{
+    return age;
+}
+
+void Dog::displayMessage() const
+{
+    cout << "狗狗名稱:" << getName() << endl
+        << "狗狗年齡:" << getAge() << endl;
+}
