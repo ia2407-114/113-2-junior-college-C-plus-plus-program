057df73e3de834480c2fe0503b4fd28e4621a0a3
VIPsheep <bb970601@gmail.com>
2025-04-15 21:51:11 +0800

    Update Source.cpp

--- 1111234035/W7_1111234035/Source.cpp
+++ 1111234035/W7_1111234035/Source.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // function main begins program execution
 int main()
 {
-    string nameOfCourse; //string¬O®w¦sÃþ§O  string of characters to store the course name
+    string nameOfCourse; // 用於儲存狗名
     GradeBook myGradeBook("Dogs Networks");
     int pass = 1234;
     // display initial value of courseName
@@ -17,9 +17,9 @@ int main()
     // prompt for, input and set course name
     cout << "\nPlease enter the course dog name:" << endl;
     getline(cin, nameOfCourse); // read a course name with blanks
-    myGradeBook.setCourseName(nameOfCourse); // set the course name
+    myGradeBook.setCourseName(nameOfCourse); // 設置狗名
 
     cout << endl; // outputs a blank line
-    myGradeBook.displayMessage(); // display message with new course name
+    myGradeBook.displayMessage(); // 輸出新的狗名
 } // end main
 
