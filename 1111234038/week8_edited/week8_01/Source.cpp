f3e97142ded3e4512551c23478b8933863b84230
andyliu300111 <andyliu300111@gmail.com>
2025-04-23 15:15:18 +0800

    week10

---
+++ 1111234038/week8_edited/week8_01/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdexcept>
+#include "Date.h"
+using namespace std;
+
+int main()
+{
+    int year, month, day;
+
+    cout << "請輸入西元年:";
+    cin >> year;
+    cout << "請輸入月份:";
+    cin >> month;
+    cout << "請輸入天:";
+    cin >> day;
+
+    Date my_d(year, month, day);
+
+    //my_d.printUniversal();
+    //cout << "\n  ";
+    //my_d.printStandard();
+
+}
