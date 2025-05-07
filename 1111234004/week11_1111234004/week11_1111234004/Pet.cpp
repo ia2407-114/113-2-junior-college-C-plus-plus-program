0e854a850f9ef29026c9b80097f45fc7521580db
1111234004 <sueyu970516@gmail.com>
2025-05-07 14:30:07 +0800

    Add files via upload

---
+++ 1111234004/week11_1111234004/week11_1111234004/Pet.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Pet.h"
+#include "Date.h"
+using namespace std;
+
+Pet::Pet(const string& name, const string& species,
+    const Date& dateOfBirth, const Date& dateOfAdopt)
+    : name(name), species(species), birthDate(dateOfBirth), adoptDate(dateOfAdopt)
+{
+    cout << "Pet建構子：" << name << ' ' << species << endl;
+}
+
+void Pet::print() const
+{
+    cout << name << ", " << species << "領養日期：";
+    adoptDate.print();
+    cout << "  出生日期：";
+    birthDate.print();
+    cout << endl;
+}
+
+Pet::~Pet()
+{
+    cout << "Pet解構子："
+        << name << ", " << species << endl;
+}
