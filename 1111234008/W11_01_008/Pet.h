05286b5428bc355875625e3139b354fac3b1b112
YuChen0613 <Vanessa97613@gmail.com>
2025-05-07 14:43:48 +0800

    Add files via upload

---
+++ 1111234008/W11_01_008/Pet.h
@@ -0,0 +1,21 @@
+#ifndef PET_H
+#define PET_H
+
+#include <string>
+#include "Date.h"
+using namespace std;
+
+class Pet
+{
+public:
+    Pet(const string&, const string&,
+        const Date&, const Date&);
+    void print() const;
+    ~Pet();
+private:
+    string name;
+    string species;
+    const Date birthDate;
+    const Date adoptDate;
+};
+#endif#pragma once
