423c0f79422358a95aa1531951166596450a713e
1223yuuu <961223yuuu@gmail.com>
2025-05-07 15:37:22 +0800

    Add files via upload

--- 1111234012/20250430/20250430/Date.cpp
+++ 1111234012/20250430/20250430/Date.cpp
@@ -1,59 +1,49 @@
 
-// Fig. 9.18: Date.cpp
-// Date class member-function definitions.
 #include <array>
 #include <iostream>
 #include <stdexcept>
-#include "Date.h" // include Date class definition
+#include "Date.h"
 using namespace std;
 
-// constructor confirms proper value for month; calls
-// utility function checkDay to confirm proper value for day
-Date::Date(int yr, int mn, int dy)
+Date::Date(int y, int m, int d)
 {
-	if (mn > 0 && mn <= 12) // validate the month
-		month = mn;
-	else
-		cout << "month must be 1 - 12" << endl;
-	year = yr;
+    if (m > 0 && m <= monthsPerYear)
+        month = m;
+    else
+        throw invalid_argument("warning：月份必須為1～12!!!!!");
 
-	day = checkDay(dy); // validate the day
+    year = y;
+    day = checkDay(d);
 
-	// output Date object to show when its constructor is called
-	cout << "Date object constructor for date ";
-	print();
-	cout << endl;
-} // end Date constructor
+    cout << "Date建構子";
+    print();
+    cout << endl;
+}
 
-// print Date object in form month/day/year
 void Date::print() const
 {
-	cout << year << '/' << month << '/' << day;
-} // end function print
+    cout << year << '/' << month << '/' << day;
+}
 
-// output Date object to show when its destructor is called
 Date::~Date()
 {
-	cout << "Date object destructor for date ";
-	print();
-	cout << endl;
-} // end ~Date destructor
+    cout << "Date解構子";
+    print();
+    cout << endl;
+}
 
-// utility function to confirm proper day value based on 
-// month and year; handles leap years, too
 unsigned int Date::checkDay(int testDay) const
 {
-	static const array< int, monthsPerYear + 1 > daysPerMonth =
-	{ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    static const array< int, monthsPerYear + 1 > daysPerMonth =
+    { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	// determine whether testDay is valid for specified month
-	if (testDay > 0 && testDay <= daysPerMonth[month])
-		return testDay;
+    if (testDay > 0 && testDay <= daysPerMonth[month])
+        return testDay;
 
-	// February 29 check for leap year 
-	if (month == 2 && testDay == 29 && (year % 400 == 0 ||
-		(year % 4 == 0 && year % 100 != 0)))
-		return testDay;
+    // February 29 檢查閏月
+    if (month == 2 && testDay == 29 && (year % 400 == 0 ||
+        (year % 4 == 0 && year % 100 != 0)))
+        return testDay;
 
-	throw invalid_argument("Invalid day for current month and year");
+    throw invalid_argument("warning：當前月份和年份的日期無效!!!!!");
 }
