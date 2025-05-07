fd0061658901dd884fab31024497c0448537449b
1111234007 <103017@mses.chc.edu.tw>
2025-05-07 14:44:18 +0800

    Add files via upload

---
+++ 1111234007/W11_07/W11_07/Pet.h
@@ -0,0 +1,24 @@
+#pragma once
+// Fig. 9.19: Employee.h
+// Employee class definition showing composition.
+#ifndef PET_H
+#define PET_H
+
+#include <string>
+#include "Date.h"
+using namespace std;
+
+class Pet
+{
+public:
+    Pet(const string&, const string&,
+        const Date&, const Date&);
+    void print() const;
+    ~Pet();
+private:
+    string name;
+    string species;
+    const Date birthDate;
+    const Date adoptDate;
+};
+#endif
