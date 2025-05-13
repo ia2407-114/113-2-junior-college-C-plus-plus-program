d18df9c31d3df442633b181482712d494ad5fd17
Katherine-wq <kate.cake.0712@gmail.com>
2025-05-13 22:30:04 +0800

    Add files via upload

---
+++ 1111234002/250507_第三題/pet.h
@@ -0,0 +1,26 @@
+﻿#ifndef PET_H
+#define PET_H
+
+#include <string>
+#include "Date.h"
+using namespace std;
+
+class Pet
+{
+public:
+    Pet(const string&, const string&,
+        const Date&, const Date&);
+
+    //·s¼WÂr¥¬¦¡start
+    Pet& setPetName(const string&, const string&);
+    //·s¼WÂr¥¬¦¡end
+
+    void print() const;
+    ~Pet();
+private:
+    string name;
+    string species;
+    const Date birthDate;
+    const Date adoptDate;
+};
+#endif#pragma once
