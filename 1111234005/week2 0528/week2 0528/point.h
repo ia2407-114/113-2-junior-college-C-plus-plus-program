890c47ea532141e86b53a1217a7ef13289cd59fb
1111234005-5 <s1111234005@nutc.edu.tw>
2025-05-28 15:48:12 +0800

    Add files via upload

---
+++ 1111234005/week2 0528/week2 0528/point.h
@@ -0,0 +1,19 @@
+#ifndef POINT_H
+#define POINT_H
+
+class Point {
+private:
+    double x, y;
+
+public:
+    Point(double x = 0, double y = 0);
+
+    // 取得 X, Y
+    double getX() const;
+    double getY() const;
+
+    // 計算距離運算子重載
+    double operator-(const Point& other) const;
+};
+
+#endif#pragma once
