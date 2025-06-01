c075c5136249e5ba49bd2c1b410668ccb1f976d8
1111234022 <zb44054@gmail.com>
2025-06-01 16:53:37 +0800

    Add files via upload

---
+++ 1111234022/兩點距離(-多載)/Point.cpp
@@ -0,0 +1,16 @@
+#include "Point.h"
+#include <cmath> 
+
+
+Point::Point(double xVal, double yVal) {
+    x = xVal;
+    y = yVal;
+}
+
+double Point::operator-(const Point& other) {
+    
+    double dx = this->x - other.x;
+    double dy = this->y - other.y;
+    
+    return sqrt(dx * dx + dy * dy);
+}
