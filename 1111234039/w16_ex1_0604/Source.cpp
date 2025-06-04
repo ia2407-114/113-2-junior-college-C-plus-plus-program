a97ff4c299efc900601c0e97996a8858cd87fb20
cheng97520 <cheng97520@gmail.com>
2025-06-04 15:05:39 +0800

    Add files via upload

---
+++ 1111234039/w16_ex1_0604/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "flight.h"
+using namespace std;
+
+int main() {
+    cout << "建立大型客機:\n";
+    airliner air1;
+    air1.display();
+    air1.display_airliner();
+
+    cout << "\n建立貨機:\n";
+    cargo cargo1;
+    cargo1.display();
+    cargo1.display_cargo();
+
+    return 0;
+}
---
+++ 1111234039/w16_ex1_0604/flight.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class flight_object {
+private:
+    string name;
+    int id;
+    int pilot;
+    float kerosene;
+
+public:
+    flight_object();
+    virtual ~flight_object();
+    void display();
+};
+
+// 大型客機類別
+class airliner : public flight_object {
+private:
+    int passenger;
+    int service_person;
+
+public:
+    airliner();
+    ~airliner();
+    void display_airliner();
+};
+
+// 貨機類別
+class cargo : public flight_object {
+private:
+    string rental_company;
+    float max_load;
+
+public:
+    cargo();
+    ~cargo();
+    void display_cargo();
+};
