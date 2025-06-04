8189d855b1c6a1d6fe5cf83c64b594c30a4c736a
1111234005-5 <s1111234005@nutc.edu.tw>
2025-06-04 15:13:16 +0800

    Add files via upload

---
+++ 1111234005/week 0604/week 0604/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "air.h"
+
+using namespace std;
+// 飛行物體類別 
+int main()
+{
+    airliner air1;
+    air1.display();
+    air1.display_airliner();
+    cout << "\n輸入貨機資料:\n";
+    cargo c1;
+    c1.display();
+    c1.display_cargo();
+
+    return 0;
+}
+
