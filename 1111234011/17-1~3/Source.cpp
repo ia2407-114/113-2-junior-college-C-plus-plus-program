7e4422a185978af31cf0102e678fe21c7d9d3425
1111234011 <julie20071001@gmail.com>
2025-06-11 15:25:50 +0800

    Add files via upload

---
+++ 1111234011/17-1~3/Source.cpp
@@ -0,0 +1,24 @@
+#include"circle.h"
+
+int main()
+{
+	cout << "¶ê»æ:\n";
+	pie_2d pie;
+	pie.compute_area();
+
+	cout << "¶êÀ@Åé:\n";
+	cron_3d cron;
+	cron.compute_v();
+
+	cout << "¶ê¬WÅé:\n";
+	cylinder_3d cc;
+	cc.compute_s();
+	cc.compute_v();
+
+	cout << "«°³ù:\n";
+	castle c1;
+	c1.cron_3d::compute_v();
+	c1.cylinder_3d::compute_v();
+	c1.compute_h();
+	c1.compute_v();
+}
---
+++ 1111234011/17-1~3/circle.h
@@ -0,0 +1,39 @@
+#ifndef C_H
+#define C_H
+#include<iostream>
+using namespace std;
+
+class circ_1d {
+public:
+	double r, pi = 3.14;
+	circ_1d();
+	void compute_circ();
+};
+class pie_2d :public circ_1d {
+public:
+	double area;
+	pie_2d();
+	void compute_area();
+};
+class cron_3d :public circ_1d {
+public:
+	double h, v;
+	cron_3d();
+	void compute_v();
+};
+class cylinder_3d :public pie_2d {
+public:
+	double h, v, s;
+	cylinder_3d();
+	void compute_v();
+	void compute_s();
+
+};
+class castle :public cron_3d, public cylinder_3d {
+public:
+	double h, v;
+	castle();
+	void compute_v();
+	void compute_h();
+};
+#endif
