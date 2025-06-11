9c0ce290a96330fc129f777b942668d13d849c65
1111234004 <sueyu970516@gmail.com>
2025-06-11 14:58:22 +0800

    Add files via upload

---
+++ 1111234004/week16_1111234004-2/geometry.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class square_2D
+{
+private:
+	int length, width;
+
+public:
+	square_2D();
+	~square_2D();
+	int area_2D();
+	void print_2D();
+};
+
+class rect_3D :public square_2D
+{
+private:
+	int height;
+
+public:
+	rect_3D();
+	~rect_3D();
+	void print_3D();
+};
