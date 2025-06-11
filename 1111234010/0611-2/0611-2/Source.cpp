04a9f06f60b363f5ec2f95299f2a60ecea20df96
1111234010 <aa0968326229aa@gmail.com>
2025-06-11 15:22:54 +0800

    Add files via upload

---
+++ 1111234010/0611-2/0611-2/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include"fo.h"
+using namespace std;
+// 飛行物體類別 
+
+int main()
+{
+    airliner air1;  // 兼具空中及水上功能   
+    air1.flight_object::display();
+    air1.display();
+
+    return 0;
+}
