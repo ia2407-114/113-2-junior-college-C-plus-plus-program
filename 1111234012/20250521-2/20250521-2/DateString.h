b0182fa2e7c13c79029dac7951d848252fbe75e5
1223yuuu <961223yuuu@gmail.com>
2025-06-11 14:43:13 +0800

    Add files via upload

---
+++ 1111234012/20250521-2/20250521-2/DateString.h
@@ -0,0 +1,18 @@
+#ifndef DATESTRING_H
+#define DATESTRING_H
+
+#include <iostream>
+#include <string>
+
+class DateString {
+    friend std::istream& operator>>(std::istream&, DateString&);
+    friend std::ostream& operator<<(std::ostream&, const DateString&);
+public:
+    std::string operator()() const { return filteredNumber; }         // 傳回 "1140521"
+    std::string operator/(char) const;                                // 格式化成 "114/05/21"
+private:
+    std::string rawString;
+    std::string filteredNumber;
+};
+
+#endif
