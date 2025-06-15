4a007c5fe51a7a565aefa5603059071ef17c1046
YuChen0613 <Vanessa97613@gmail.com>
2025-06-15 23:05:34 +0800

    Add files via upload

---
+++ 1111234008/W14_03_008/Source.cpp
@@ -0,0 +1,30 @@
+﻿﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1("apple");
+    string s2("banana");
+
+    cout << "s1¬O\"" << s1 << "\"; s2¬O\"" << s2 << "\""
+        << "\n\n¤ñ¸ûs1»Ps2"
+        << "\ns1 == s2 yields " << (s1 == s2 ? "V" : "X")
+        << "\ns1 != s2 yields " << (s1 != s2 ? "V" : "X")
+        << "\ns1 >  s2 yields " << (s1 > s2 ? "V" : "X")
+        << "\ns1 <  s2 yields " << (s1 < s2 ? "V" : "X")
+        << "\ns1 >= s2 yields " << (s1 >= s2 ? "V" : "X")
+        << "\ns1 <= s2 yields " << (s1 <= s2 ? "V" : "X");
+
+    string s3("abple");
+    string s4("aanana");
+
+    cout << "\n\ns3¬O\"" << s3 << "\"; s4¬O\"" << s4 << "\""
+        << "\n\n¤ñ¸ûs3»Ps4"
+        << "\ns3 == s4 yields " << (s3 == s4 ? "V" : "X")
+        << "\ns3 != s4 yields " << (s3 != s4 ? "V" : "X")
+        << "\ns3 >  s4 yields " << (s3 > s4 ? "V" : "X")
+        << "\ns3 <  s4 yields " << (s3 < s4 ? "V" : "X")
+        << "\ns3 >= s4 yields " << (s3 >= s4 ? "V" : "X")
+        << "\ns3 <= s4 yields " << (s3 <= s4 ? "V" : "X");
+}
